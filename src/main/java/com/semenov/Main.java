88da4aef800fcac33e66f428c204937a6cc90abd
SemenovRustam <zeevvsss@yandex.ru>
2022-07-15 09:55:11 +0700

    add check money

--- src/main/java/com/semenov/Main.java
+++ src/main/java/com/semenov/Main.java
@@ -13,31 +13,38 @@ import java.util.concurrent.Executors;
 public class Main {
     public static final int NUMBER_OF_THREADS = 2;
     public static final int MONEY = 10_000;
+    public static final int NUMBER_OF_TRANSACTION = 30;
 
     public static void main(String[] args) {
         ExecutorService executorService = Executors.newFixedThreadPool(NUMBER_OF_THREADS);
-
+        Transaction transaction = new Transaction();
         Account account1 = createAccount();
         Account account2 = createAccount();
         Account account3 = createAccount();
         Account account4 = createAccount();
+
         List<Account> accountList = List.of(account1, account2, account3, account4);
-        Transaction transaction = new Transaction();
+        int moneyBeforeTransaction = checkMoney(accountList);
 
         List<CompletableFuture<Void>> futureList = new ArrayList<>();
-        for (int i = 0; i < 30; i++) {
+        for (int i = 0; i < NUMBER_OF_TRANSACTION; i++) {
             futureList.add(CompletableFuture.runAsync(() -> transaction.transferMoney(account1, account2, Utils.createRandomAmount()), executorService));
             futureList.add(CompletableFuture.runAsync(() -> transaction.transferMoney(account3, account4, Utils.createRandomAmount()), executorService));
         }
         futureList.forEach(CompletableFuture::join);
         executorService.shutdown();
 
-        int totalMoney = accountList.stream()
+        int moneyAfterTransaction = checkMoney(accountList);
+
+        log.info("Total amount on all accounts before transactions= {} $", moneyBeforeTransaction);
+        log.info("Total amount on all accounts after transactions= {} $", moneyAfterTransaction);
+        log.info("Program completes successfully !");
+    }
+
+    private static int checkMoney(List<Account> accountList) {
+        return accountList.stream()
                 .mapToInt(Account::getMoney)
                 .sum();
-
-        log.info("total amount on all accounts = {} $", totalMoney);
-        log.info("Program completes successfully !");
     }
 
     private static Account createAccount() {
